Olzhas0986-AOSP/android_device_xiaomi_munch

munch: Birth of Munch Device Tree
* Reference: https://github.com/LineageOS/android_device_xiaomi_munch/tree/5106fab34a20eaece6178232c93c40eb404ac868

Signed-off-by: olzhas0986 <kdiroljas@gmail.com>

lineage_munch.mk
@@ -1,0 +1,27 @@
+#
+# Copyright (C) 2021-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from munch device
+$(call inherit-product, device/xiaomi/munch/device.mk)
+
+PRODUCT_NAME := lineage_munch
+PRODUCT_DEVICE := munch
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F4
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="munch-user 13 RKQ1.211001.001 V816.0.9.0.ULMMIXM release-keys" \
+    BuildFingerprint=POCO/munch_global/munch:13/RKQ1.211001.001/V816.0.9.0.ULMMIXM:user/release-keys

Olzhas0986-AOSP/device_xiaomi_munch

lineage_munch.mk
@@ -1,0 +1,27 @@
+#
+# Copyright (C) 2021-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from munch device
+$(call inherit-product, device/xiaomi/munch/device.mk)
+
+PRODUCT_NAME := lineage_munch
+PRODUCT_DEVICE := munch
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F4
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="munch-user 13 RKQ1.211001.001 V816.0.9.0.ULMMIXM release-keys" \
+    BuildFingerprint=Redmi/munch/munch:13/RKQ1.211001.001/V816.0.9.0.ULMMIXM:user/release-keys